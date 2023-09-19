33ec645e1db6e6bcdb12647374abecc9f531da2b
nwest12303 <114024389+nwest12303@users.noreply.github.com>
2023-09-18 21:51:44 -0400

    Created new activity for the coninue screen and added basic buttons to the xml file.

---
+++ app/src/main/java/com/example/cs2340cteam50game/ContinueScreen.java
@@ -0,0 +1,35 @@
+package com.example.cs2340cteam50game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class ContinueScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_continue_screen);
+        ImageButton continueButton = (ImageButton) findViewById(R.id.continueButton);
+        ImageButton backButton = (ImageButton) findViewById(R.id.back);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(ContinueScreen.this, );
+                //startActivity(intent);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(ContinueScreen.this, ConfigScreen.class);
+               // startActivity(intent);
+            }
+        });
+    }
+}
