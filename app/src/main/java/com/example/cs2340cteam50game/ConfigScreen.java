064e10504617b7c9a4a5b7371405fc292e336577
jbuchsbaum03 <buckysjosh@gmail.com>
2023-09-20 19:52:01 -0400

    Polished the start screen and cleaned up some code

--- app/src/main/java/com/example/cs2340cteam50game/ConfigScreen.java
+++ app/src/main/java/com/example/cs2340cteam50game/ConfigScreen.java
@@ -1,7 +1,10 @@
 package com.example.cs2340cteam50game;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
@@ -11,7 +14,7 @@ public class ConfigScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.config_screen);
 
 
         Button button1 = (Button) findViewById(R.id.button2);
@@ -23,5 +26,16 @@ public class ConfigScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        SharedPreferences prefs1 = getSharedPreferences("playerChoices", MODE_PRIVATE);
+        SharedPreferences.Editor prefEditor = getSharedPreferences("playerChoices", MODE_PRIVATE).edit();
+        //Save user inputs into the sharedPreferences
+        prefEditor.apply();
+
+        Log.d("Prefs Saved", "Player preferences successfully saved");
     }
 }
--- app/src/main/java/com/example/cs2340cteam50game/OpeningScreen.java
+++ app/src/main/java/com/example/cs2340cteam50game/OpeningScreen.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Process;
-import android.util.Log;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
@@ -15,28 +13,15 @@ public class OpeningScreen extends AppCompatActivity {
     private int button1Count = 0;
     private boolean buttonClicked = false;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        SharedPreferences.Editor e1 = getSharedPreferences("p1", MODE_PRIVATE).edit();
-        e1.clear();
-        e1.apply();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.opening_screen);
 
         Button exitButton = (Button) findViewById(R.id.exitButton);
         Button nextButton = (Button) findViewById(R.id.startButton);
         TextView textView1 = (TextView) findViewById(R.id.textView1);
-        SharedPreferences p1 = getSharedPreferences("p1", MODE_PRIVATE);
-
-        if (p1.contains("text")) {
-            textView1.setText(p1.getString("text", ""));
-            button1Count = p1.getInt("clickCount", 0);
-        }
 
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,18 +38,6 @@ public class OpeningScreen extends AppCompatActivity {
             }
         });
 
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        TextView text = (TextView) findViewById(R.id.textView1);
-        SharedPreferences p1 = getSharedPreferences("p1", MODE_PRIVATE);
-        SharedPreferences.Editor e1 = getSharedPreferences("p1", MODE_PRIVATE).edit();
-        e1.putString("text", text.getText().toString());
-        e1.putInt("clickCount", button1Count);
-        e1.apply();
-        Log.d("Activity Stopped", "A1 Closed");
     }
 
 }
