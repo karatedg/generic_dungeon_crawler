94a7a94b742cf70d4f0989a8a3bea5d494f03ae0
vedsathwik275 <vedanth.sathwik@gmail.com>
2023-10-11 19:32:55 -0400

    Formatted time and date

--- app/src/main/java/com/example/cs2340cteam50game/LeaderBoardView.java
+++ app/src/main/java/com/example/cs2340cteam50game/LeaderBoardView.java
@@ -5,20 +5,27 @@ import android.widget.TextView;
 public class LeaderBoardView {
 
     public static void renderLeaderboard(Leaderboard l, TextView text, Score score) {
-        String render = String.format("%10s %10s\n", "NAME", "SCORE");
+        String render = String.format("%10s %10s %30s\n", "NAME", "SCORE", "DATE/TIME");
+
 
         Score latest = score;
-        render += String.format("%10s %10d\n\n", latest.getName(), latest.getScore());
+
+        String time = latest.getTimestamp().toString();
+        render += String.format("%10s %15d %45s\n\n", latest.getName(), latest.getScore(),
+                time.replace('T', ' ').substring(0, time.length() - 7));
 
         int leaderBoardSize = Leaderboard.getLeaderboard().getSize();
         if(leaderBoardSize > 5) {
             leaderBoardSize = 5;
         }
 
+
         for (int i = 0; i < leaderBoardSize; i++) {
             if (i < l.getScores().size()) {
                 Score s = l.getScores().get(i);
-                render += String.format("%10s %10d\n", s.getName(), s.getScore());
+                String sTime = s.getTimestamp().toString();
+                render += String.format("%10s %15d %45s\n\n", s.getName(), s.getScore(),
+                        sTime.replace('T', ' ').substring(0, time.length() - 7));
             } else {
                 render += String.format("%10s %10s\n", "-", "-");
             }
