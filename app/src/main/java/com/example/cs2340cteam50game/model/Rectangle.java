81e2c5ccc356658ece79be5231610c96d60e29a6
jbuchsbaum03 <buckysjosh@gmail.com>
2023-10-22 16:46:15 -0400

    Replaced RectF with a new Rectangle class to handle collisions due to issues with JUnits.

---
+++ app/src/main/java/com/example/cs2340cteam50game/model/Rectangle.java
@@ -0,0 +1,15 @@
+package com.example.cs2340cteam50game.model;
+
+public class Rectangle {
+    public float left;
+    public float top;
+    public float right;
+    public float bottom;
+
+    public Rectangle(float left, float top, float right, float bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+}
